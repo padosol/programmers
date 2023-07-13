b07fa7045231cacc04343608994a122695f7caa3
padosol <109058030+padosol@users.noreply.github.com>
2023-07-13 22:13:55 +0900

    [level 0] Title: 짝수는 싫어요, Time: 1.76 ms, Memory: 72.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120813. 짝수는 싫어요/짝수는 싫어요.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class Solution {
+    public int[] solution(int n) {
+        
+        
+        List<Integer> answer = new ArrayList<>();
+        
+        for(int i=1;i<=n;i++){
+            if(i % 2 != 0){
+                answer.add(i);
+            }
+        }
+        
+        return answer.stream().mapToInt(a -> a).toArray();
+    }
+}
