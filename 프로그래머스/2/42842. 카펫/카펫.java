f2504d5ca52d158748367233f7abd83b710c9ddd
padosol <109058030+padosol@users.noreply.github.com>
2024-05-08 21:53:14 +0900

    [level 2] Title: 카펫, Time: 0.01 ms, Memory: 71.5 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.java
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -1,23 +1,25 @@
 class Solution {
     public int[] solution(int brown, int yellow) {
+        // 클수 없다 라는 조건이 있음.
         int[] answer = new int[2];
 
-        for(int i=1;i*i<=yellow;i++) {
+        for(int i=1;i * i <= yellow; i++) {
+
+            // 사각형이 충족함
             if(yellow % i == 0) {
 
-                int first = i;
-                int second = yellow / i;
+                int y = i;          // 세로
+                int x = yellow / i; // 가로
 
-                int sum = first * 2 + second * 2 + 4 + first * second;
+                int sum = x * 2 + y * 2 + 4 + x * y; // 총 타일 갯수
 
-                if(sum == yellow + brown) {
-                    answer[0] = second + 2;
-                    answer[1] = first + 2;
+                if( sum ==  brown + yellow) {
+                    answer[0] = x + 2;
+                    answer[1] = y + 2;
                     break;
                 }
             }
         }
-
 
         return answer;
     }
