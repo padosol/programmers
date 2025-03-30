1b8537dfa12041e2e44f9cb5e677123900d7d7e6
한상훈 <109058030+padosol@users.noreply.github.com>
2025-03-30 15:36:51 +0900

    [Gold V] Title: 평범한 배낭, Time: 1780 ms, Memory: 158936 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
@@ -1,17 +1,24 @@
+N, K = map(int, input().split())
+w_v = [list(map(int,input().split())) for _ in range(N)]
+
 def recur(idx, weight):
-    if weight > K :
-        return -99999
-    if idx == N :
-        return 0 
+    if weight > K:
+        return -9999999
+
+    if idx == N:
+        return 0
+
     if dp[idx][weight] != -1:
         return dp[idx][weight]
-    
-    dp[idx][weight] = max(recur(idx + 1, weight + items[idx][0]) + items[idx][1], recur(idx + 1, weight))
+
+    dp[idx][weight] = max(
+        recur(idx + 1, weight + w_v[idx][0]) + w_v[idx][1],
+        recur(idx + 1, weight)
+    )
+
     return dp[idx][weight]
 
-N,K = map(int,input().split())
-items = [list(map(int,input().split())) for _ in range(N)]
-#무게를 고려해서 가치를 담는 dp테이블, 무게는 얼마든지 커질 수 있으므로 10만, 그리고 물건개수만큼 N 작성
+
 dp = [[-1 for _ in range(100_001)] for _ in range(N)]
 
-print(recur(0,0))
+print(recur(0, 0))
