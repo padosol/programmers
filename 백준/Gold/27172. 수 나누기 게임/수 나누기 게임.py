4c61bc366377adc03166b3bfed85b1bf1100417c
한상훈 <109058030+padosol@users.noreply.github.com>
2025-07-31 12:36:27 +0900

    [Gold IV] Title: 수 나누기 게임, Time: 344 ms, Memory: 144472 KB -BaekjoonHub

---
+++ 백준/Gold/27172. 수 나누기 게임/수 나누기 게임.py
@@ -0,0 +1,17 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+x = list(map(int, input().rstrip().split()))
+S = set(x)
+M = max(x)
+sieve = [0 for _ in range(M+1)]
+for i in x:
+    if i == M: continue
+    for j in range(i,M+1,i):
+        if j in S:
+            sieve[i] += 1
+            sieve[j] -= 1
+
+for i in x:
+    print(sieve[i], end=' ')
