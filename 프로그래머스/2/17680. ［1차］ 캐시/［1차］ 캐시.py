6fe2ec4ef93d3eba8dbb31b1eebc13b2bff5dd88
한상훈 <109058030+padosol@users.noreply.github.com>
2025-09-22 01:16:08 +0900

    [level 2] Title: [1차] 캐시, Time: 75.66 ms, Memory: 16.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.py
@@ -0,0 +1,16 @@
+from collections import deque
+def solution(cacheSize, cities):
+    q = deque(maxlen=cacheSize)
+    answer = 0
+    for city in cities:
+        city = city.lower()
+
+        if city in q:
+            q.remove(city)
+            q.append(city)
+            answer += 1
+        else:
+            q.append(city)
+            answer += 5
+        
+    return answer
