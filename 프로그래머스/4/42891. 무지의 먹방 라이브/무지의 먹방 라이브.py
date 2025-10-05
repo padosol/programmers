e82ce3decdafedb0866f9bab75870e4a25959537
한상훈 <109058030+padosol@users.noreply.github.com>
2025-10-05 22:05:02 +0900

    [level 4] Title: 무지의 먹방 라이브, Time: 239.23 ms, Memory: 43 MB -BaekjoonHub

---
+++ 프로그래머스/4/42891. 무지의 먹방 라이브/무지의 먹방 라이브.py
@@ -0,0 +1,24 @@
+def solution(food_times, k):
+    foods = []
+    n = len(food_times) # 음식의 갯수
+    for i in range(n):
+        foods.append((food_times[i], i+1))
+
+    foods.sort()
+
+    pretime = 0
+    for i, food in enumerate(foods):
+        diff = food[0] - pretime
+        if diff != 0:
+            spend = diff * n
+            if spend <= k:
+                k -= spend
+                pretime = food[0]
+            else:
+                k %= n
+                sorted_foods = sorted(foods[i:], key=lambda x:x[1])
+
+                return sorted_foods[k][1]
+        n -= 1
+
+    return -1
