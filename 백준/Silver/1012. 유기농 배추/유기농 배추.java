48acea2128dd3e59f934197fa0fbdcf5106c15c9
SangHan <109058030+padosol@users.noreply.github.com>
2026-04-07 20:30:55 +0900

    [Silver II] Title: 유기농 배추, Time: 140 ms, Memory: 15812 KB -BaekjoonHub

---
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.java
@@ -0,0 +1,63 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int T;
+    static int M, N, K;
+    static int[][] arr;
+    static boolean visited[][];
+
+    public static void main(String[] args) throws NumberFormatException, IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        T = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < T; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            M = Integer.parseInt(st.nextToken());
+            N = Integer.parseInt(st.nextToken());
+            K = Integer.parseInt(st.nextToken());
+
+            arr = new int[M][N];
+            visited = new boolean[M][N];
+
+            for (int j = 0; j < K; j++) {
+                st = new StringTokenizer(br.readLine());
+                int x = Integer.parseInt(st.nextToken());
+                int y = Integer.parseInt(st.nextToken());
+                arr[x][y] = 1;
+            }
+
+            int count = 0;
+            for (int x = 0; x < M; x++) {
+                for (int y = 0; y < N; y++) {
+                    if (arr[x][y] == 1 && !visited[x][y]) {
+                        dfs(x, y);
+                        count++;
+                    }
+                }
+            }
+            System.out.println(count);
+        }
+    }
+
+    static int[] dx = { 1, -1, 0, 0 };
+    static int[] dy = { 0, 0, 1, -1 };
+
+    public static void dfs(int x, int y) {
+        visited[x][y] = true;
+
+        for (int i = 0; i < 4; i++) {
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+
+            if (0 <= nx && nx < M && 0 <= ny && ny < N && !visited[nx][ny] && arr[nx][ny] == 1) {
+                dfs(nx, ny);
+            }
+        }
+
+    }
+
+}
